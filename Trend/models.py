4c310d1c3edf5c3d60b6a10e51f938cdfee11286
ayukha <ayussh.khare@gmail.com>
2017-11-12 15:14:18 +0530

    Models Added

--- Trend/models.py
+++ Trend/models.py
@@ -4,3 +4,38 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Doctors(models.Model):
+	user = models.onetooneField(User, on_delete=models.CASCADE,null=True)
+	degree = models.IntegerField()
+	City = models.CharField(maxlength=40)
+	State = models.CharField(maxlength=40)
+	Country = models.CharField(maxlength=40)
+
+
+
+
+class Disease(models.Model):
+	Name = models.CharField(maxlength=40,blank=False)
+	Description = models.TextField(null=False,blank=False)
+	Precaution = models.TextField(null=False,blank=False)
+	Symptoms = models.TextField(null=False,blank=False)
+
+
+	def __str__(self):
+        return unicode(self.Name)
+
+
+
+class Choice(models.Model):
+	disease_name =models.ForeignKey(Disease, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+
+    def __str__(self):
+        return unicode(self.choice_text)
+
+
--- Trend/views.py
+++ Trend/views.py
@@ -3,4 +3,28 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.views import generic
+
 # Create your views here.
+
+from .models import timezone
+
+class IndexView(generic.ListView):
+	tmeplate_name = "######"
+	context_object_name = #####
+
+
+
+class DetailView(generic.DetailView):
+	model = 
+	template_name = "######"
+
+
+class ResultView(generic.DetailView):
+	model =  ######
+	tmeplate_name = #####
+
+
+def Vote(request, ########)
+
+
